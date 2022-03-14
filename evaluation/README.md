4f9452f4ed424a620edda3842ae448c04bf4588b
Khuyagbaatar Batsuren <khuyagbaatar@gmail.com>
2022-03-14 21:54:10 +0800

    Evaluation tool on word-level task

---
+++ evaluation/README.md
@@ -0,0 +1,59 @@
+The official evaluation script lives in this directory. We have provided sample output from the baseline model on the development data in ../baseline/. You may run the baseline as shown in the examples below.
+
+Word-level Task Evaluation:
+
+python evaluate.py --guess ../baseline/eng.word.dev.bert.tsv --gold ../data/eng.word.dev.tsv --category
+
+category:       101
+levenshtein:    1.56
+precision:      51.59
+recall: 48.8
+f-measure:      50.16
+
+category:       111
+levenshtein:    3.28
+precision:      34.86
+recall: 27.45
+f-measure:      30.71
+
+category:       011
+levenshtein:    3.35
+precision:      32.77
+recall: 27.16
+f-measure:      29.7
+
+category:       010
+levenshtein:    2.76
+precision:      25.31
+recall: 33.91
+f-measure:      28.99
+
+category:       000
+levenshtein:    2.16
+precision:      2.42
+recall: 6.63
+f-measure:      3.55
+
+category:       110
+levenshtein:    3.31
+precision:      24.95
+recall: 26.21
+f-measure:      25.56
+
+category:       001
+levenshtein:    1.66
+precision:      43.76
+recall: 49.5
+f-measure:      46.45
+
+category:       100
+levenshtein:    2.79
+precision:      12.49
+recall: 19.58
+f-measure:      15.25
+
+category:       all
+levenshtein:    2.72
+precision:      20.86
+recall: 28.28
+f-measure:      24.01
---
+++ evaluation/evaluate_word.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+"""
+Official Evaluation Script for the SIGMORPHON 2022 Morpheme Segmentation Shared Task.
+Returns precision, recall, f-measure, and mean Levenhstein distance.
+Author: Khuyagbaatar Batsuren
+Last Update: 03/14/2022
+"""
+
+import numpy as np
+import codecs
+
+def distance(str1, str2):
+    """Simple Levenshtein implementation for evalm."""
+    m = np.zeros([len(str2)+1, len(str1)+1])
+    for x in range(1, len(str2) + 1):
+        m[x][0] = m[x-1][0] + 1
+    for y in range(1, len(str1) + 1):
+        m[0][y] = m[0][y-1] + 1
+    for x in range(1, len(str2) + 1):
+        for y in range(1, len(str1) + 1):
+            if str1[y-1] == str2[x-1]:
+                dg = 0
+            else:
+                dg = 1
+            m[x][y] = min(m[x-1][y] + 1, m[x][y-1] + 1, m[x-1][y-1] + dg)
+    return int(m[len(str2)][len(str1)])
+
+def read(fname, flag):
+    """ read file name """
+    D = {}
+    C = {}
+    with codecs.open(fname, 'rb', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if flag:
+              lemma, segments, tag = line.split("\t")
+              C[lemma] = tag
+            else:
+              lemma, segments = line.split("\t")[0:2]
+            D[lemma] = segments.replace(' @@','|')
+    return D, C
+
+def printNumbers(T):
+  answers, D_cat = T[0], T[1]
+  cat_set = set(D_cat)
+  for cat in cat_set:
+    print("\ncategory:\t" +cat)
+    print ("levenshtein:\t"+str(answers[cat]['distance'])+"\nprecision:\t"+str(answers[cat]['precision']))
+    print ("recall:\t"+str(answers[cat]['recall'])+"\nf-measure:\t"+str(answers[cat]['fmeasure']))
+
+def eval_form(gold, guess, category):
+    """ compute average accuracy and edit distance for word-level task """
+    correct, dist, total = {}, {}, {}
+    tp, fp, total_m, total_g = {}, {}, {}, {}
+    answers = {}
+    if len(category) < 1:
+      category['']='all'
+    for cat in category.values():
+      tp[cat] = .0
+      dist[cat] = .0
+      total_m[cat] = .0
+      total_g[cat] = .0
+      total[cat] = .0
+    for lemma, str1 in gold.items():
+      str2 = u"" # empty string if no guess
+      cat = "all"
+      if lemma in category:
+        cat = category[lemma]
+      if (cat in total) is False:
+        total[cat] = .0
+      if lemma in guess:
+        str2 = guess[lemma]
+        gold_set = set(str1.split('|'))
+        guess_list = str2.split('|')
+        if (cat in total_g) is False:
+          total_g[cat] = .0
+        if (cat in total_m) is False:
+          total_m[cat] = .0
+        total_g[cat] += len(str2.split('|'))
+        total_m[cat] += len(str1.split('|'))
+        
+      for b in guess_list:
+          if b in gold_set:
+            if (cat in tp) is False:
+              tp[cat]=.0
+            tp[cat] += 1
+      dist[cat] += distance(str1, str2)
+      total[cat] += 1
+
+    for cat in total.keys():
+      answers[cat] = {}
+      answers[cat]['distance'] =round(dist[cat]/total[cat],2)
+      answers[cat]['precision'] = round((tp[cat]/total_g[cat])*100,2)
+      answers[cat]['recall'] = round((tp[cat]/total_m[cat])*100,2)
+      answers[cat]['fmeasure'] = round((2*answers[cat]['precision']*answers[cat]['recall']) / (answers[cat]['precision']+answers[cat]['recall']),2)
+    return (answers, total.keys())
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='SIGMORPHON 2022 Morpheme Segmentation Shared Task Evaluation')
+    parser.add_argument("--gold", help="Gold standard", required=True, type=str)
+    parser.add_argument("--guess", help="Model output", required=True, type=str)
+    parser.add_argument("--category", help="Morphological category", default=False,  action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()    
+
+    D_gold, D_cat = read(args.gold, args.category)
+    D_guess = read(args.guess, False)[0]
+    if args.category:
+      printNumbers(eval_form(D_gold, D_guess,D_cat))
+    printNumbers(eval_form(D_gold, D_guess,{}))
